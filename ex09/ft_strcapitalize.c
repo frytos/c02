f3667aace1469ef78eacd01e3123372d2ff96ab7
frytos <frytos@protonmail.com>
2024-02-17 07:10:23 +0100

    misc

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,13 +6,31 @@
 /*   By: argrouss <argrouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 07:45:00 by argrouss          #+#    #+#             */
-/*   Updated: 2024/02/14 07:46:32 by argrouss         ###   ########.fr       */
+/*   Updated: 2024/02/14 23:58:39 by argrouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_strcapitalize(char *str);
+char *ft_strlowcase(char *str);
 
 char *ft_strcapitalize(char *str)
 {
+	
+}
 
+
+char *ft_strlowcase(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != 0)
+	{
+		if (str[index] >= 65 && str[index] <= 90)
+		{
+			str[index] = str[index] + 32;
+		}
+		index++;
+	}
+	return (str);
 }
